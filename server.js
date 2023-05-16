8e3f41218a326f4e39fe296c514a36cfe3eb0166
halima570 <84968500+halima570@users.noreply.github.com>
2023-05-16 13:09:36 +0100

    Update server.js rss

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 
 require("./index");
 dotenv.config();
-//require("./rss");
+require("./rss");
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
